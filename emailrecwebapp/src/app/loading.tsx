f9cbb95979c11905532608250dd8c15a933da0fa
Kevin Han <khan@colgate.edu>
2023-06-08 22:16:34 +0800

    implemented loading

---
+++ emailrecwebapp/src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>loading...
+        <svg className='animate-spin h-5 w-5'></svg>
+    </div>
+  )
+}
+
+export default Loading
