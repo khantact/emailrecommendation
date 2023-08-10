c4eab0bdc5add748e964a8c85326e2148580d263
Jaanhvi Agarwal <jaanhviagarwal@jaanhvis-air.colgate.edu>
2023-08-10 15:19:41 -0400

    ignore

--- emailrecwebapp/src/app/fetchData/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
-import '../../utils/firebase';
-import { useRouter } from 'next/navigation';
-import '../../utils/authUtils';
-
